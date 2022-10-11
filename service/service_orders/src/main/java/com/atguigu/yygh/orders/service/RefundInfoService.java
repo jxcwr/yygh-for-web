b85fb0418610b3291e8c158f90ebcc9b48367d91
jxc <1225100315@aliyun.com>
2022-10-11 17:17:57 +0800

    取消预约的功能实现

---
+++ service/service_orders/src/main/java/com/atguigu/yygh/orders/service/RefundInfoService.java
@@ -0,0 +1,8 @@
+package com.atguigu.yygh.orders.service;/**
+ * @description TODD
+ * @author jxc
+ * @version 1.0
+ * @date 2022年10月11日 11:19
+ */
+public class RefundInfoService {
+}
---
+++ service/service_orders/src/main/java/com/atguigu/yygh/orders/service/impl/RefundInfoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.atguigu.yygh.orders.service.impl;/**
+ * @description TODD
+ * @author jxc
+ * @version 1.0
+ * @date 2022年10月11日 11:20
+ */
+public class RefundInfoServiceImpl {
+}
