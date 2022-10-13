893c0d4449b5950828e7594a9a383f8979e7e8ad
jxc <1225100315@aliyun.com>
2022-10-13 09:26:50 +0800

    定时任务的编写

---
+++ service/service_orders/src/main/java/com/atguigu/yygh/orders/listener/MQListener.java
@@ -0,0 +1,8 @@
+package com.atguigu.yygh.orders.listener;/**
+ * @description TODD
+ * @author jxc
+ * @version 1.0
+ * @date 2022年10月12日 22:10
+ */
+public class MQListener {
+}
---
+++ service/service_task/src/main/java/com/atgugui/yygh/task/job/PatientRemindJob.java
@@ -0,0 +1,13 @@
+package com.atgugui.task.job;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jxc
+ * @version 1.0
+ * @description TODD就诊人提醒，定时任务
+ * @date 2022年10月11日 20:15
+ */
+@Component
+public class PatientRemindJob {
+}
